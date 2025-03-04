df2d5dae4f75e9d0194e8826da63831faa3253f2
magdump <pirjademl7@gmail.com>
2025-03-04 10:57:32 +0530

    feat : add starting point problems in linkedlist

---
+++ problems/linked-list/detect-loop-in-linked-list.go
@@ -0,0 +1,15 @@
+package linkedlist
+
+func DetectLoop(head *Node) bool {
+	slow := head
+	fast := head
+	for (fast != nil) && (fast.Next != nil) {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+
+	}
+	return false
+}
---
+++ problems/linked-list/find-starting-point-in-loop.go
@@ -0,0 +1,23 @@
+package linkedlist
+
+func StartingPointOfLoop(head *Node) *Node {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			slow = head
+			for slow == fast {
+				slow = slow.Next
+				fast = fast.Next
+			}
+			return slow
+		}
+
+	}
+	return nil
+}
